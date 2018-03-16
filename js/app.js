8b05c8a48636f25d16edbf060de4145c5030905e
Mark Perry <mark.j.perry11@gmail.com>
2018-03-15 18:46:45 -0700

    Add player class

--- js/app.js
+++ js/app.js
@@ -19,6 +19,33 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 5 * 80;
+}
+
+Player.prototype.handleInput = function(direction) {
+    var moves = {
+        left: -103,
+        right: 103,
+        up: -83,
+        down: 83,
+    }
+    
+    if (direction && direction === 'right' || direction === 'left') {
+        this.x += moves[direction];
+    } else if (direction) {
+        this.y += moves[direction];
+    }
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -29,9 +56,12 @@ Enemy.prototype.render = function() {
 // Place the player object in a variable called player
 
 let allEnemies = [];
+
 allEnemies.push(new Enemy(1, 'fast'));
 allEnemies.push(new Enemy(2, 'normal'));
 allEnemies.push(new Enemy(3, 'slow'));
+
+let player = new Player();
 // allEnemies.push(new Enemy('right', 20, null));
 
 
